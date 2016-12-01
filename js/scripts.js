56b05a4b87fc4249d787be19d9bc3248913b26c3
wchengong <wchengong@vcd-ch204-05.eastern.ewu.edu>
2016-12-01 13:42:42 -0800

    multiple cities

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,49 @@
 // My Scripts
+
+
+//spokane
+$.simpleWeather({
+    location: '99203',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.spokane .temp').text(weather.temp);
+      $('.spokane .city').text(weather.city);
+      $('.spokane img').attr('src', weather.image);
+      $('.spokane .title').text(weather.title);
+  
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>NO Weather!</p>');
+    }
+  
+  });
+
+
+//seattle
+$.simpleWeather({
+    location: '98105',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.seattle .temp').text(weather.temp);
+      $('.seattle .city').text(weather.city);
+      $('.seattle img').attr('src', weather.image);
+      $('.seattle .title').text(weather.title);
+  
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>NO Weather!</p>');
+    }
+  
+  });
