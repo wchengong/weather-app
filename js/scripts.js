3f27d53dc525a45a4c623aae589ae33030711386
wchengong <wchengong@vcd-ch204-05.eastern.ewu.edu>
2016-12-01 14:44:46 -0800

    geo and weather icon

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 //spokane
 $.simpleWeather({
     location: '99203',
+    woeid: '',
     unit: 'f',
     success: function(weather) {
       
@@ -13,7 +14,14 @@ $.simpleWeather({
       $('.spokane img').attr('src', weather.image);
       $('.spokane .title').text(weather.title);
   
-      
+      //If this condition show this icon 
+        if ( 26 >= weather.code && 30 <= weather.code ){
+            $('.spokane .wi').addClass('wi-day-cloudy');
+        
+        }
+        
+        
+        
       // Entire weather object
       console.log(weather);
     },
@@ -28,6 +36,7 @@ $.simpleWeather({
 //seattle
 $.simpleWeather({
     location: '98105',
+    woeid: '',
     unit: 'f',
     success: function(weather) {
       
@@ -36,8 +45,7 @@ $.simpleWeather({
       $('.seattle .city').text(weather.city);
       $('.seattle img').attr('src', weather.image);
       $('.seattle .title').text(weather.title);
-  
-      
+   
       // Entire weather object
       console.log(weather);
     },
@@ -47,3 +55,42 @@ $.simpleWeather({
     }
   
   });
+
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo button').click( function() {
+     
+    //load weather using your lat/lng coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude+','+position.coords.longitude); 
+    });
+   
+});
+
+
+// Get geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.geo .temp').text(weather.temp);
+      $('.geo .city').text(weather.city);
+      $('.geo img').attr('src', weather.image);
+      $('.geo .title').text(weather.title);
+     
+        
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+    
+};
